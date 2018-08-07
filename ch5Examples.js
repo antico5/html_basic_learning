55c111feda07bd4db814d3c466bc0214ecb287ed
facundofpaz <facundofpaz@gmail.com>
2018-08-07 15:25:53 -0300

    Se agregaron mas ejemplos

--- ch5Examples.js
+++ ch5Examples.js
@@ -9,11 +9,46 @@ repeat = (n, action) => {
 
 mostrarCuadrado = num => console.log(num * num)
 
+repeat(5, mostrarCuadrado)
 
 let labels = []
 
 repeat(5, i => labels.push(i)) //la funcion q paso como parametro la declaro ahi
 
 
-//console.log(labels)
-repeat(5, mostrarCuadrado)
+console.log(labels)
+//recibe un numero, crea una f q dice cuando el arg es >
+mayorQue = num => { return mayor => mayor > num}
+
+let mayorQueAlgo = mayorQue(10) //f q dice si arg es > 10
+console.log(typeof mayorQueAlgo) // function
+console.log(mayorQueAlgo(11))
+
+unless = (test, entonces) => {if (!test) entonces() }
+
+repeat(5, n => { 
+	unless(n % 2 == 1, () => {
+		console.log(n, "is even")
+		})
+})
+
+let array = ["Hola", "Como", "andas", 1, [4, 3, 2, 1]], array2 = []
+array.forEach(i => array2.push(i))
+console.log(array2)
+
+filtrar = (array, filtro) => {
+  
+  let filtrados = []
+  for (let i of array) {
+    if (filtro(i)) {
+      filtrados.push(i);
+    }
+  }
+  return filtrados;
+}
+//filtra aquellos elementos de array q sean cadenas
+console.log(filtrar(array, e => typeof e == "string"))
+//del lenguaje
+let array3 = array2.filter( e => typeof e == "string")
+
+console.log(array3.map( m => m.toUpperCase()))
